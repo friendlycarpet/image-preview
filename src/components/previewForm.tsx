fe29af13ff42d05d215df5829ef9665df4b17503
Programmer <programmer@example.com>
2022-06-02 21:22:07 +0800

    added img

--- src/components/previewForm.tsx
+++ src/components/previewForm.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import styles from "../styles/Picture.module.css";
+import background from "../../public/background.png";
+import Image from 'next/image'
+
+// const PreviewForm = () => {
+//   return (
+    // <article className={styles.article}>
+    //   <picture className={styles.picture}>
+    //     <source media="(min-width: 0px)" srcSet={background} />
+    //     <Image src={background} alt="background" />
+    //   </picture>
+    //   <h1 className={styles.header}>React Is Awesome</h1>
+    // </article>
+//   );
+// };
+
+// export default PreviewForm;
 
 function PreviewForm() {
   // type NewsletterPost = {
@@ -27,6 +44,7 @@ function PreviewForm() {
     register,
     handleSubmit,
     watch,
+    getValues,
     formState: { errors }
   } = useForm({
     defaultValues: {
@@ -39,11 +57,46 @@ function PreviewForm() {
   });
 
   console.log(watch("id")); // you can watch individual input by pass the name of the input
-
   return (
+    
     <div className="grid place-items-center h-screen">
+
+
+      
+        <article className={styles.article}>
+          <picture className={styles.picture}>
+            <source media="(min-width: 0px)" srcSet={background} />
+            <Image src={background} alt="background" />
+          </picture>
+          {/* <h1 className={styles.header}>{getValues('id')}</h1> */}
+          {/* <h2>{getValues('createdAt')}</h2> */}
+          {/* <Image src={getValues('cover_img_url')}  width="50px" height = "50px" alt="background" /> */}
+          {/* <Image src={'https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg'}  width="50px" height = "50px" alt="background" /> */}
+
+
+          <h1 className={styles.cover_img_url}>
+          <Image src={'https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg'}  width="50px" height = "50px" alt="background" />
+          </h1>
+
+          {/* <h1 className={styles.header}>
+          <Image src='https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg'  width="50px" height = "50px" alt="background" />
+          </h1> */}
+          <h1 className={styles.createdAt}>31 Oct 2021</h1>
+          <h1 className={styles.id}>Vitalik Posted</h1>
+
+          <h1 className={styles.slug}>Crypto Cities</h1>
+          <h1 className={styles.categories}>#futurism, #governance, #crypto</h1>
+
+
+          {/* {(getValues('cover_img_url').includes('wikimedia.org')) &&    <h1 className={styles.header}>
+          <Image src={getValues('cover_img_url')}  width="50px" height = "50px" alt="background" />
+          </h1>} */}
+
+        </article>
       <form className="w-full max-w-sm" onSubmit={handleSubmit((data) => {
         alert(JSON.stringify(data));
+        console.log(getValues("slug"))
+        console.log('cover img url', errors.cover_img_url)
       })}>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/3">
@@ -63,6 +116,8 @@ function PreviewForm() {
           </div>
           <div className="md:w-2/3">
             <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-categories" type="text" placeholder="What kind of bear is best?" {...register("slug",{ required: true })}/>
+            {errors.slug && <p>This field is required</p>}
+
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
@@ -103,89 +158,11 @@ function PreviewForm() {
             </button>
           </div>
         </div>
-        {errors.id && errors.slug && errors.categories && errors.createdAt && errors.cover_img_url && <p>All fields are required</p>}
+        {(errors.id && errors.slug && errors.categories && errors.createdAt && errors.cover_img_url) && <p>All fields are required</p>}
 
       </form >
     </div>
   );
 }
-
-
-// return (
-//   <div className="grid place-items-center h-screen">
-//     <form className="w-full max-w-sm" onSubmit={handleSubmit((data) => {
-//       alert(JSON.stringify(data));
-//     })}>
-//       <div className="md:flex md:items-center mb-6">
-//         <div className="md:w-1/3">
-//         <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
-//             Author
-//           </label>
-//         </div>
-//         <div className="md:w-2/3">
-//           <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Dwight Schrute" {...register("id")}/>
-//         </div>
-//       </div>
-//       <div className="md:flex md:items-center mb-6">
-//         <div className="md:w-1/3">
-//           <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-slug">
-//             Title
-//           </label>
-//         </div>
-//         <div className="md:w-2/3">
-//           <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-categories" type="text" placeholder="What kind of bear is best?" />
-//         </div>
-//       </div>
-//       <div className="md:flex md:items-center mb-6">
-//         <div className="md:w-1/3">
-//           <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-article-categories">
-//             Categories
-//           </label>
-//         </div>
-//         <div className="md:w-2/3">
-//           <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-categories" type="text" placeholder="bears,beets,battlestar-galactica" />
-//         </div>
-//       </div>
-//       <div className="md:flex md:items-center mb-6">
-//         <div className="md:w-1/3">
-//           <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-createdAt">
-//             Created At
-//           </label>
-//         </div>
-//         <div className="md:w-2/3">
-//           <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-createdAt" type="text" placeholder="26 April 2007" />
-//         </div>
-//       </div>
-//       <div className="md:flex md:items-center mb-6">
-//         <div className="md:w-1/3">
-//           <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="cover-img-url">
-//             Cover Image
-//           </label>
-//         </div>
-//         <div className="md:w-2/3">
-//         <input className="form-control     block    w-full    px-3    py-1.5    text-base    font-normal    text-gray-700    bg-white bg-clip-padding    border border-solid border-gray-300    rounded    transition    ease-in-out    m-0    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="file" id="cover-img-url"/>
-//         </div>
-//       </div>
-//       <div className="md:flex md:items-center mb-6">
-//         <div className="md:w-1/3" />
-//         <label className="md:w-2/3 block text-gray-500 font-bold">
-//           <input className="mr-2 leading-tight" type="checkbox" />
-//           <span className="text-sm">
-//             Send me your newsletter!
-//           </span>
-//         </label>
-//       </div>
-//       <div className="md:flex md:items-center">
-//         <div className="md:w-1/3" />
-//         <div className="md:w-2/3">
-//           <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
-//             Create Image
-//           </button>
-//         </div>
-//       </div>
-//     </form >
-//   </div>
-// );
-// }
 
 export default PreviewForm;
