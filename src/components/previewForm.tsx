a7bef6191e7cda14cb7c3c9f369b23a361cbf41b
Programmer <programmer@example.com>
2022-06-03 22:57:09 +0800

    cleanup and new domain

--- src/components/previewForm.tsx
+++ src/components/previewForm.tsx
@@ -1,45 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import { useForm } from "react-hook-form";
 import styles from "../styles/Picture.module.css";
 import background from "../../public/background.png";
 import Image from 'next/image'
 
-// const PreviewForm = () => {
-//   return (
-    // <article className={styles.article}>
-    //   <picture className={styles.picture}>
-    //     <source media="(min-width: 0px)" srcSet={background} />
-    //     <Image src={background} alt="background" />
-    //   </picture>
-    //   <h1 className={styles.header}>React Is Awesome</h1>
-    // </article>
-//   );
-// };
-
-// export default PreviewForm;
 
 function PreviewForm() {
-  // type NewsletterPost = {
-  //   id: string,
-  //   slug: string,
-  //   categories: string[],
-  //   createdAt: string,
-  //   cover_img_url: string,
-  // }
-  // const initialPost: NewsletterPost = {
-  //   id: "",
-  //   slug: "",
-  //   categories: [],
-  //   createdAt: "",
-  //   cover_img_url: '',
-  // }
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   watch,
-  //   formState: { errors }
-  // } = useForm<NewsletterPost>();
-
+  const [renderImg, setRenderImg] = useState(false);
   const {
     register,
     handleSubmit,
@@ -58,45 +25,27 @@ function PreviewForm() {
 
   console.log(watch("id")); // you can watch individual input by pass the name of the input
   return (
-    
     <div className="grid place-items-center h-screen">
-
-
-      
+      {renderImg && 
         <article className={styles.article}>
           <picture className={styles.picture}>
             <source media="(min-width: 0px)" srcSet={background} />
             <Image src={background} alt="background" />
           </picture>
-          {/* <h1 className={styles.header}>{getValues('id')}</h1> */}
-          {/* <h2>{getValues('createdAt')}</h2> */}
-          {/* <Image src={getValues('cover_img_url')}  width="50px" height = "50px" alt="background" /> */}
-          {/* <Image src={'https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg'}  width="50px" height = "50px" alt="background" /> */}
+   
+          
+          {(getValues('cover_img_url').includes('wikimedia.org') || getValues('cover_img_url').includes('as1.ftcdn.net')) &&    <h1 className={styles.cover_img_url}>
+          <Image src={getValues('cover_img_url')}  width="300px" height = "300px" alt="background" />
+          </h1>}
+          <h1 className={styles.createdAt}>{getValues('createdAt')}</h1>
+          <h1 className={styles.id}>{getValues('id')}</h1>
+          <h1 className={styles.slug}><i>{getValues('slug')}</i></h1>
+          <h1 className={styles.categories}>{getValues('categories')}</h1>
 
-
-          <h1 className={styles.cover_img_url}>
-          <Image src={'https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg'}  width="50px" height = "50px" alt="background" />
-          </h1>
-
-          {/* <h1 className={styles.header}>
-          <Image src='https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg'  width="50px" height = "50px" alt="background" />
-          </h1> */}
-          <h1 className={styles.createdAt}>31 Oct 2021</h1>
-          <h1 className={styles.id}>Vitalik Posted</h1>
-
-          <h1 className={styles.slug}>Crypto Cities</h1>
-          <h1 className={styles.categories}>#futurism, #governance, #crypto</h1>
-
-
-          {/* {(getValues('cover_img_url').includes('wikimedia.org')) &&    <h1 className={styles.header}>
-          <Image src={getValues('cover_img_url')}  width="50px" height = "50px" alt="background" />
-          </h1>} */}
-
-        </article>
+        </article>}
       <form className="w-full max-w-sm" onSubmit={handleSubmit((data) => {
         alert(JSON.stringify(data));
-        console.log(getValues("slug"))
-        console.log('cover img url', errors.cover_img_url)
+        setRenderImg(true)
       })}>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/3">
@@ -116,8 +65,6 @@ function PreviewForm() {
           </div>
           <div className="md:w-2/3">
             <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-categories" type="text" placeholder="What kind of bear is best?" {...register("slug",{ required: true })}/>
-            {errors.slug && <p>This field is required</p>}
-
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
