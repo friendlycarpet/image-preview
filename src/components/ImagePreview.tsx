f68e4041f96787d38885da57008cbc7f8e0ababb
Programmer <programmer@example.com>
2022-06-05 17:21:27 +0800

    export stuff

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -5,41 +5,101 @@ import Image from 'next/image'
 import domtoimage from 'dom-to-image';
 
 const ImagePreview = (props) => {
-    const container = useRef(null);
+    let [previewPic, setPreviewPic] = useState(null);
 
-    function exportToPng(dom) {
-        domtoimage
-          .toPng(dom)
-          .then(function (dataUrl) {
-            image.onload = function () {
-              resolve(image);
-          };
-          image.onerror = reject;
-          image.src = encodeURI(uri);
-            document.body.appendChild(img);
-          })
-          .catch(function (error) {
-            console.error("oops, something went wrong!", error);
-          });
+    const container = useRef(null);
+    // function exportToPng(dom) {
+    //     domtoimage
+    //       .toPng(dom)
+    //       .then(function (dataUrl) {
+    //         image.onload = function () {
+    //           resolve(image);
+    //       };
+    //       image.onerror = reject;
+    //       image.src = encodeURI(uri);
+    //         document.body.appendChild(img);
+    //       })
+    //       .catch(function (error) {
+    //         console.error("oops, something went wrong!", error);
+    //       });
+    //   }
+
+      function exportToPng(dom) {
+        previewPic = React.cloneElement(dom)
+        // domtoimage
+        //   .toPng(dom)
+        //   .then(function (dataUrl) {
+        //     image.onload = function () {
+        //       resolve(image);
+        //   };
+        //   image.onerror = reject;
+        //   image.src = encodeURI(uri);
+        //     document.body.appendChild(img);
+        //   })
+        //   .catch(function (error) {
+        //     console.error("oops, something went wrong!", error);
+        //   });
       }
       function exportToBlob(dom) {
-        domtoimage
-        .toJpeg(dom, { quality: 0.95 })
-        .then(function (dataUrl) {
-          var link = document.createElement("a");
-          link.download = "my-image-name.jpeg";
-          link.href = dataUrl;
-          link.click();
-        });
+        previewPic = React.cloneElement(dom)
+
+        // domtoimage
+        //   .toPng(dom)
+        //   .then(function (dataUrl) {
+        //     image.onload = function () {
+        //       resolve(image);
+        //   };
+        //   image.onerror = reject;
+        //   image.src = encodeURI(uri);
+        //     document.body.appendChild(img);
+        //   })
+        //   .catch(function (error) {
+        //     console.error("oops, something went wrong!", error);
+        //   });
       }
-    
+        // useEffect(function mount() {
+        //   function onScroll() {
+        //     console.log("scroll!");
+        //   }
+      
+        //   window.addEventListener("scroll", onScroll);
+      
+        //   return function unMount() {
+        //     window.removeEventListener("scroll", onScroll);
+        //   };
+        // });      
+
+        useEffect(() => {
+            // domtoimage
+            //     .toJpeg(previewPic, { quality: 0.95 })
+            //     .then(function (dataUrl) {
+            //     link = document.createElement("a");
+            //     link.download = "my-image-name.jpeg";
+            //     link.href = dataUrl;
+            //     link.click();
+            //     });
+            domtoimage
+            .toPng(previewPic)
+            .then(function (dataUrl) {
+              image.onload = function () {
+                resolve(image);
+            };
+            image.onerror = reject;
+            image.src = encodeURI(uri);
+              document.body.appendChild(img);
+            })
+            .catch(function (error) {
+              console.error("oops, something went wrong!", error);
+            });
+        }, [previewPic])
+
     return (
         <div>
         <article className={styles.article} ref={container}>
           {/* {console.log('container current', container.current)}
           {console.log('article', document.getElementById('Picture_article__737IL'))}
           {console.log('articles', Array.from(document.querySelectorAll("article")))} */}
-           <button onClick={() => exportToPng(container.current)}>export</button>
+           <button onClick={() => exportToBlob(container.current)}>export</button>
 
           <picture className={styles.picture}>
             <source media="(min-width: 0px)" srcSet={background} />
