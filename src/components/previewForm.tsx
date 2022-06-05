79eaac40bf5a7a67fa6fe9db463012e6af676b41
Programmer <programmer@example.com>
2022-06-05 18:04:59 +0800

    back to original

--- src/components/previewForm.tsx
+++ src/components/previewForm.tsx
@@ -1,36 +1,34 @@
-import React, {useState, useRef, useEffect} from "react";
-import { useForm } from "react-hook-form";
-import styles from "../styles/Picture.module.css";
-import background from "../../public/background.png";
-import Image from 'next/image'
-import ImagePreview from "./ImagePreview";
+import React, { useState, useRef, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import ImagePreview from './ImagePreview';
 
 function PreviewForm() {
   const [renderImg, setRenderImg] = useState(false);
   const {
     register,
     handleSubmit,
-    watch,
     getValues,
-    formState: { errors }
+    formState: { errors },
   } = useForm({
     defaultValues: {
-      id: "",
-      slug: "",
-      categories: "",
-      createdAt: "",
-      cover_img_url: ""
-    }
+      id: '',
+      slug: '',
+      categories: '',
+      createdAt: '',
+      cover_img_url: '',
+    },
   });
-  
+
   return (
     <div className="grid place-items-center h-screen">
-      {renderImg && 
-       <ImagePreview createdAt={getValues('createdAt')} id={getValues('id')}  slug={getValues('slug')} categories={getValues('categories')} cover_img_url={getValues('cover_img_url')}></ImagePreview>}
-      <form className="w-full max-w-sm" onSubmit={handleSubmit((data) => {
-        alert(JSON.stringify(data));
-        setRenderImg(true)
-      })}>
+      {renderImg
+       && <ImagePreview createdAt={getValues('createdAt')} id={getValues('id')} slug={getValues('slug')} categories={getValues('categories')} cover_img_url={getValues('cover_img_url')} />}
+      <form
+        className="w-full max-w-sm"
+        onSubmit={handleSubmit((data) => {
+          setRenderImg(true);
+        })}
+      >
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/3">
             <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
@@ -38,7 +36,7 @@ function PreviewForm() {
             </label>
           </div>
           <div className="md:w-2/3">
-            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Dwight Schrute" {...register("id",{ required: true })}/>
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Dwight Schrute" {...register('id', { required: true })} />
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
@@ -48,7 +46,7 @@ function PreviewForm() {
             </label>
           </div>
           <div className="md:w-2/3">
-            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-categories" type="text" placeholder="What kind of bear is best?" {...register("slug",{ required: true })}/>
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-categories" type="text" placeholder="What kind of bear is best?" {...register('slug', { required: true })} />
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
@@ -58,7 +56,7 @@ function PreviewForm() {
             </label>
           </div>
           <div className="md:w-2/3">
-            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-categories" type="text" placeholder="bears,beets,battlestar-galactica" {...register("categories",{ required: true })} />
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-categories" type="text" placeholder="bears,beets,battlestar-galactica" {...register('categories', { required: true })} />
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
@@ -68,7 +66,7 @@ function PreviewForm() {
             </label>
           </div>
           <div className="md:w-2/3">
-            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-createdAt" type="text" placeholder="26 April 2007" {...register("createdAt",{ required: true })} />
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-createdAt" type="text" placeholder="26 April 2007" {...register('createdAt', { required: true })} />
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
@@ -78,7 +76,7 @@ function PreviewForm() {
             </label>
           </div>
           <div className="md:w-2/3">
-            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-coverImgUrl" type="text" placeholder="https://en.wikipedia.org/wiki/Dwight_Schrute#/media/File:Dwight_Schrute.jpg" {...register("cover_img_url",{ required: true })} />
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-coverImgUrl" type="text" placeholder="https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg" {...register('cover_img_url', { required: true })} />
           </div>
         </div>
         <div className="md:flex md:items-center">
@@ -90,7 +88,7 @@ function PreviewForm() {
           </div>
         </div>
         {(errors.id && errors.slug && errors.categories && errors.createdAt && errors.cover_img_url) && <p>All fields are required</p>}
-      </form >
+      </form>
     </div>
   );
 }
